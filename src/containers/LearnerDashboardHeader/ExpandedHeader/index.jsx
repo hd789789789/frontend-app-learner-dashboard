1df5769a01fed3b331d88a7068d24eab00c8ba32
hd789789789 <65296761+hd789789789@users.noreply.github.com>
2024-03-12 07:57:57 +0700

    Add Vietnamese Language

--- src/containers/LearnerDashboardHeader/ExpandedHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/index.jsx
@@ -1,46 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { getConfig } from '@edx/frontend-platform';
-import { useIntl } from '@edx/frontend-platform/i18n';
-import { Button } from '@edx/paragon';
+import { getConfig } from "@edx/frontend-platform";
+import { useIntl } from "@edx/frontend-platform/i18n";
+import { Button } from "@edx/paragon";
 
-import WidgetNavbar from 'containers/WidgetContainers/WidgetNavbar';
-import urls from 'data/services/lms/urls';
-import { reduxHooks } from 'hooks';
-import { EXPANDED_NAVBAR } from 'widgets/RecommendationsPaintedDoorBtn/constants';
+import WidgetNavbar from "containers/WidgetContainers/WidgetNavbar";
+import urls from "data/services/lms/urls";
+import { reduxHooks } from "hooks";
+import { EXPANDED_NAVBAR } from "widgets/RecommendationsPaintedDoorBtn/constants";
 
-import AuthenticatedUserDropdown from './AuthenticatedUserDropdown';
-import { useIsCollapsed, findCoursesNavClicked } from '../hooks';
-import messages from '../messages';
-import BrandLogo from '../BrandLogo';
+import AuthenticatedUserDropdown from "./AuthenticatedUserDropdown";
+import { useIsCollapsed, findCoursesNavClicked } from "../hooks";
+import messages from "../messages";
+import BrandLogo from "../BrandLogo";
 
 export const ExpandedHeader = () => {
-  const { formatMessage } = useIntl();
-  const { courseSearchUrl } = reduxHooks.usePlatformSettingsData();
-  const isCollapsed = useIsCollapsed();
+    const { formatMessage } = useIntl();
+    const { courseSearchUrl } = reduxHooks.usePlatformSettingsData();
+    const isCollapsed = useIsCollapsed();
 
-  const exploreCoursesClick = findCoursesNavClicked(
-    urls.baseAppUrl(courseSearchUrl),
-  );
+    const exploreCoursesClick = findCoursesNavClicked(urls.baseAppUrl(courseSearchUrl));
 
-  if (isCollapsed) {
-    return null;
-  }
+    if (isCollapsed) {
+        return null;
+    }
 
-  return (
-    <header className="d-flex shadow-sm align-items-center learner-variant-header pl-4">
-      <div className="flex-grow-1 d-flex align-items-center">
-        <BrandLogo />
+    return (
+        <header className="d-flex shadow-sm align-items-center learner-variant-header pl-4">
+            <div className="flex-grow-1 d-flex align-items-center">
+                <BrandLogo />
 
-        <Button
-          as="a"
-          href="/"
-          variant="inverse-primary"
-          className="p-4 course-link"
-        >
-          {formatMessage(messages.course)}
-        </Button>
-        {/* <Button
+                <Button as="a" href="/" variant="inverse-primary" className="p-4 course-link">
+                    {formatMessage(messages.course)}
+                </Button>
+                {/* <Button
           as="a"
           href={urls.programsUrl()}
           variant="inverse-primary"
@@ -48,30 +41,25 @@ export const ExpandedHeader = () => {
         >
           {formatMessage(messages.program)}
         </Button> */}
-        {/* <Button
-          as="a"
-          href={urls.baseAppUrl(courseSearchUrl)}
-          variant="inverse-primary"
-          className="p-4"
-          onClick={exploreCoursesClick}
-        >
-          {formatMessage(messages.discoverNew)}
-        </Button> */}
-        <WidgetNavbar placement={EXPANDED_NAVBAR} />
-        <span className="flex-grow-1" />
-        <Button
-          as="a"
-          href={getConfig().SUPPORT_URL}
-          variant="inverse-primary"
-          className="p-4"
-        >
-          {formatMessage(messages.help)}
-        </Button>
-      </div>
+                <Button
+                    as="a"
+                    href={urls.baseAppUrl(courseSearchUrl)}
+                    variant="inverse-primary"
+                    className="p-4"
+                    onClick={exploreCoursesClick}
+                >
+                    {formatMessage(messages.discoverNew)}
+                </Button>
+                <WidgetNavbar placement={EXPANDED_NAVBAR} />
+                <span className="flex-grow-1" />
+                <Button as="a" href={getConfig().SUPPORT_URL} variant="inverse-primary" className="p-4">
+                    {formatMessage(messages.help)}
+                </Button>
+            </div>
 
-      <AuthenticatedUserDropdown />
-    </header>
-  );
+            <AuthenticatedUserDropdown />
+        </header>
+    );
 };
 
 ExpandedHeader.propTypes = {};
